69b8c06563c5afc5356b2ccfa5e2e13ebd852eeb
Saqib Razzaq <saqibrazzaq@gmail.com>
2021-09-20 23:25:53 +0500

    cat and mouse

---
+++ HackerRank/HackerRank/Algorithms/Implementation/CatsandaMouse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.Implementation
+{
+    class CatsandaMouse
+    {
+        public static void Test()
+        {
+            int x = 1;
+
+            int y = 3;
+
+            int z = 2;
+
+            string result = catAndMouse(x, y, z);
+
+            Console.WriteLine(result);
+        }
+
+        private static string catAndMouse(int x, int y, int z)
+        {
+            if (Math.Abs(x - z) < Math.Abs(y - z))
+                return "Cat A";
+            else if (Math.Abs(y - z) < Math.Abs(x - z))
+                return "Cat B";
+            else
+                return "Mouse C";
+        }
+    }
+}
--- HackerRank/HackerRank/Program.cs
+++ HackerRank/HackerRank/Program.cs
@@ -11,7 +11,7 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            BillDivision.Test();
+            CatsandaMouse.Test();
         }
     }
 }
